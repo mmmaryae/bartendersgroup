6a5113485a6ce5ae5d3f4d68cc78efa2f5cb3400
maria <150175935+mmmaryae@users.noreply.github.com>
2026-05-20 18:05:16 -0300

    arrumando conexão

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,16 +1,12 @@
 <?php
-// Configurações do Banco de Dados
-$host = 'localhost'; // Servidor
-$usuario = 'root'; // Usuário padrão do XAMPP
-$senha = ''; // Por padrão, não tem senha no XAMPP
-$banco = 'bar_virtual'; // Substitua pelo nome do banco que você criou
+$host = 'sql212.infinityfree.com';
+$usuario = 'if0_41975847';
+$senha = 'Mari280404';
+$banco = 'if0_41975847_bartendersgroup';
 
-// Criar a conexão
 $conexao = new mysqli($host, $usuario, $senha, $banco);
 
-// Verificar a conexão
 if ($conexao->connect_error) {
     die("Erro na conexão com o banco de dados: " . $conexao->connect_error);
 }
-
 ?>
